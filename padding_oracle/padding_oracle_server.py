49d7a9d0fc3e5e52394613503eabc252b0ad7a84
ttay <ttay@umich.edu>
2019-05-06 21:02:33 -0400

    finished the decryptor

--- padding_oracle/padding_oracle_server.py
+++ padding_oracle/padding_oracle_server.py
@@ -1,4 +1,5 @@
 from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
 from Cryptodome.Util import Padding
 from base64 import b16encode, b16decode
 import argparse
@@ -23,18 +24,24 @@ class PaddingOracle:
     class DecryptBehavior:
         INVALID_PADDING = 0
         INVALID_VALIDATION = 1
-        VALID_VALIDATION = 1
-        OTHER_ERROR = 3
-
+        VALID_VALIDATION = 2
 
     def __init__(self, key):
-        if len(key.encode()) is not 16:
-            raise KeyLenError("Key must be exactly 16 bytes long")
-        self.key = key.encode()
+        KEY_SIZE = 16
+        if len(key.encode()) is KEY_SIZE:
+            self.key = key.encode()
+        elif len(key.encode()) is 0:
+            self.key = get_random_bytes(KEY_SIZE)
+        else:
+            raise KeyLenError("Key must be exactly 16 bytes long or empty")
 
     @staticmethod
     def genHash(sender, recipient):
         return ":" + sender + ":" + recipient
+
+    @staticmethod
+    def getHash(hashed_pt, hash_len):
+        return hashed_pt[-hash_len:]
 
     def encrypt(self, sender, recipient, plain_text):
         """
@@ -58,15 +65,22 @@ class PaddingOracle:
         """
         Decrypts an AES encrypted cipher
         """
-        init_vector = b16decode(cipher_text[:AES.block_size])
-        ct_bytes = b16decode(cipher_text[AES.block_size:])
+        init_vector = b16decode(cipher_text)[:AES.block_size]
+        ct_bytes = b16decode(cipher_text)[AES.block_size:]
         cipher = AES.new(self.key, AES.MODE_CBC, iv= init_vector)
+        padded_hashed_pt = cipher.decrypt(ct_bytes)
         try:
-            padded_hashed_pt = cipher.decrypt(ct_bytes)
+            hashed_pt = Padding.unpad(padded_hashed_pt, AES.block_size).decode()
+            hash_len = len(sender) + len(recipient) + 2 #two colons
+            pt_MAC = self.getHash(hashed_pt, hash_len)
+            if pt_MAC == self.genHash(sender, recipient):
+                # Hash is valid
+                return PaddingOracle.DecryptBehavior.VALID_VALIDATION
+            else:
+                return PaddingOracle.DecryptBehavior.INVALID_VALIDATION
         except ValueError:
-            # Decryption failure
-            return DecryptBehavior.INVALID_VALIDATION
-
+            # Padding error
+            return PaddingOracle.DecryptBehavior.INVALID_PADDING
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- tests.py
+++ tests.py
@@ -4,3 +4,5 @@ KEY = "DEADBEEFPOTATATO"
 server = PaddingOracle(KEY)
 encrypted_message = server.encrypt("ham", "burr", "I like cats")
 print(encrypted_message)
+ret_code = server.decrypt("ham", "burr", encrypted_message)
+print(ret_code)
